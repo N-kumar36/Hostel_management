ecbc3ba19d5482fc53fc043856d266678d45342e
N-kumar36 <nitya3666@gmail.com>
2026-07-02 20:40:46 +0530

    Website fix

--- Loadbalancer/src/config/servers.js
+++ Loadbalancer/src/config/servers.js
@@ -1,17 +1,22 @@
 export const servers = [
+    // Ramkrishna36@gmail.com
     {
         id: 1,
         url: "https://hostel-app-backend.vercel.app/",
         active: true
     },
+
+    // nityakumarbarman116@gmail.com
     {
         id: 2,
         url: "https://hostel-management-three-roan.vercel.app",
         active: true
     },
-    {
-        id: 3,
-        url: "https://hostel-management-rouge-six.vercel.app",
-        active: true
-    }
+
+    // Nitya3666@gmail.com account
+    // {
+    //     id: 3,
+    //     url: "https://hostel-management-rouge-six.vercel.app",
+    //     active: true
+    // }
 ];
